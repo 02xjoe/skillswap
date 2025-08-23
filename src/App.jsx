04048e8547078e1e4dabbe7165901c49e6b28473
02xjoe <johntobif@gmail.com>
2025-08-23 09:19:59 +0100

    rffgrv

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "tailwindcss"
-import './index.css'
+import {Routes,Route,Navigate} from 'react-router-dom'
+import Navbar from  './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/login.jsx'
+import Register from './pages/Register.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Profile from './pages/Profile.jsx'
+import ProtectedRoute from './components/HProtectedRoute.jsx'
+import {AuthProvider} from './context/AuthContent.jsx'
 
-export default function App() {
-  const [count, setCount] = useState(0)
-
+export default function App(){
   return (
-    <div>
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind v4
-      </h1>
-      <p className="text-red-500">If this is red, Tailwind works!</p>
-    </div>
+    <AuthProvider>
+      <div className="min-h-screen bg-gray=50">
+        <Navbar/>
+        <main className='max-w-6xl mx-auto p-4'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route
+            path='/dashboard'
+            element={
+              <ProtectedRoute>
+                <Dashboard/>
+              </ProtectedRoute>
+            }
+            />
+            <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile/>
+              </ProtectedRoute>
+            }
+            />
+            <Route path='*' element={<Navigate to='/'replace/>}/>
+          </Routes>
+        </main>
+      </div>
+    </AuthProvider>
   )
 }
   
