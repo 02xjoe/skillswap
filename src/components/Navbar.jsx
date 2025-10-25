f2a9ab693eff3d066e6a5453da02dc5c2dc0dac2
02xjoe <johntobif@gmail.com>
2025-10-25 23:13:46 +0100

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ export default function Navbar() {
           </Link>
         </div>
 
-        {/* Auth Buttons */}
+        {/* Auth Buttonss*/}
         <div className="flex space-x-4">
           {user ? (
             <>
